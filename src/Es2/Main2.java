aeb39378183def168379e901b82a3d4bb70679ad
federicopignatelli <federicopignatelli@icloud.com>
2023-12-05 16:41:23 +0100

    es1

--- src/Es2/Main2.java
+++ src/Es2/Main2.java
@@ -1,4 +1,36 @@
 package Es2;
+import java.util.*;
+
 
 public class Main2 {
+    public static void main(String[] args) {
+
+//        Random random = new Random();
+//        for(int i = 0; i < 100; i++) {
+//            int numero = random.nextInt(101);
+//            listaNumeri.add(numero);
+//        }
+//        System.out.println(listaNumeri);
+
+        List<Integer> listaNumeri = new ArrayList<>();
+        generatoreNumeriCasuali(listaNumeri, 10);
+        invertiLista(listaNumeri);
+    }
+
+    public static void generatoreNumeriCasuali (List<Integer> listaNumeri, int lunghezza){
+        Random random = new Random();
+        for(int i = 0; i < lunghezza; i++) {
+            int numero = random.nextInt(101);
+            listaNumeri.add(numero);
+        }
+
+        System.out.println(listaNumeri);
+    }
+
+    public static void invertiLista (List<Integer> listaNumeri){
+        Collections.reverse(listaNumeri);
+        System.out.println(listaNumeri);
+    }
+
+
 }
